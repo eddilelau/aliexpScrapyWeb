6af5b68795540f3ecad54a9dc59b03e455497386
eddilelau <395407702@qq.com>
2019-08-11 18:59:40 +0800

    "modified the monitorProduct manage method 2019.08.11"

--- website_aliData/test.py
+++ website_aliData/test.py
@@ -15,8 +15,28 @@ from django.db.models import Count
 
 client = pymongo.MongoClient(MONGO_URI)
 db = client[MONGO_DB_otherSale]
+#tag /comment /competingproductnum/max_productId/past1_sales/past2_sales
+date=datetime.date.today().strftime("%m/%d/%Y")
+print(date,type(date))
+tags = competingProductInfo.objects.values_list('tag',flat=True).distinct()
 
-tag='MP1'
-tagInformation=list(monitorProductTag.objects.filter(tag=tag).values('tag', 'comment'))[0]
+# tagsInfo={item['tag']:item['comment'] for item in monitorProductTag.objects.values('tag','comment')}
+# tagsProducts = {key:competingProductInfo.objects.filter(tag=key).values_list('productId',flat=True) for key,value in tagsInfo.items()}
+# tagsBestSales={tag:competingProductDailySalesforFiveDays.objects.filter(productId__in=productIds,date=date).order_by('-past1_Sales')[0] for tag,productIds in tagsProducts.items()}
+# tagTables=[]
+# for tag in tagsInfo.keys():
+#     tagTables.append(
+#         {
+#             'tag':tag,
+#             'comment':tagsInfo[tag],
+#             'productIdNum':len(tagsProducts[tag]),
+#             'max_productId':tagsBestSales[tag].productId,
+#             'max_productId_past1_Sales': tagsBestSales[tag].past1_Sales,
+#             'max_productId_past2_Sales': tagsBestSales[tag].past2_Sales,
+#             'max_productId_past3_Sales': tagsBestSales[tag].past3_Sales,
+#             'max_productId_past4_Sales': tagsBestSales[tag].past4_Sales,
+#
+#         }
+#     )
 
-print("tag",tag)
+print(len(tagTables),tagTables)
